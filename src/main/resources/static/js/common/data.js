267426463c0141ac26c374751e6d388204d727d1
Xiuming Lee <lxmcoder@163.com>
2020-12-18 17:26:27 +0800

    home page build

--- src/main/resources/static/js/common/data.js
+++ src/main/resources/static/js/common/data.js
@@ -1,30 +1,11 @@
 let blog_data = [
     {
         articleType:"原创",
-        title:"抓取Bilibili哔哩哔哩网站视频(Java和Python双版本实现)",
-        summary:"我一直觉得编程语言只是一种工具，重要的是思想。",
-        date:"2020-05-10 10:15:10",
-        readNum: 10593,
-        commentNum: 22,
-        hrefUrl: "https://blog.csdn.net/XiumingLee/article/details/106014889"
-    },
-    {
-        articleType:"原创",
-        title:"抓取Bilibili哔哩哔哩网站视频(Java和Python双版本实现)",
-        summary:"我一直觉得编程语言只是一种工具，重要的是思想。",
-        date:"2020-05-10 10:15:10",
+        title:"构建出现问题，点击这里查看更多文章",
+        summary:"构建出现问题，点击这里查看更多文章",
+        date:new Date(),
         readNum: 0,
-        commentNum: 22,
-        hrefUrl: "https://blog.csdn.net/XiumingLee/article/details/106014889"
-    },
-    {
-        articleType:"原创",
-        title:"抓取Bilibili哔哩哔哩网站视频(Java和Python双版本实现)",
-        summary:"我一直觉得编程语言只是一种工具，重要的是思想。",
-        date:"2020-05-10 10:15:10",
-        readNum: 10593,
         commentNum: 0,
-        hrefUrl: "https://blog.csdn.net/XiumingLee/article/details/106014889"
-    },
-
+        hrefUrl: "https://blog.csdn.net/XiumingLee"
+    }
 ]
