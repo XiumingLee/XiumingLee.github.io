ef8976f600848c7c37a17d3fe403f02376ad367a
Xiuming Lee <lxmcoder@gmail.com>
2021-04-04 11:48:39 +0800

    修改数据爬取代码

--- src/main/java/cn/xiuminglee/home/page/util/BlogUtils.java
+++ src/main/java/cn/xiuminglee/home/page/util/BlogUtils.java
@@ -23,7 +23,8 @@ public class BlogUtils {
         String blogHtml = HttpUtil.get(CSDN_BLOG_URL);
         //    解析字符串
         Document document = Jsoup.parse(blogHtml);
-        Elements elements = document.select("#articleMeList-blog > div.article-list > div");
+        //Elements elements = document.select("#articleMeList-blog > div.article-list > div");
+        Elements elements = document.select("#floor-user-profile_485 > div > div.user-profile-body > div > div.user-profile-body-right > div > div:nth-child(2) > div > div > div");
         elements.forEach(element -> {
             if (blogInfoList.size() >= 10){
                 return;
@@ -36,30 +37,44 @@ public class BlogUtils {
     }
 
     private static void buildBlogInfo(Element element,BlogInfo blogInfo){
-        Element aElement = element.selectFirst("h4 > a");
+        //Element aElement = element.selectFirst("h4 > a");
+        Element aElement = element.selectFirst("article > a");
         // href
         String href = aElement.attr("href");
         blogInfo.setHrefUrl(href);
+
+
         // title
-        String title = aElement.ownText();
+        //String title = aElement.ownText();
+        Element titleElement = aElement.selectFirst("div.blog-list-box-top > h4");
+        String title = titleElement.ownText();
         blogInfo.setTitle(title);
+
+
         // articleType
-        String articleType = element.selectFirst("h4 > a > span").ownText();
+        //String articleType = element.selectFirst("h4 > a > span").ownText();
+        String articleType = aElement.selectFirst("div.blog-list-footer > div.blog-list-footer-left > div.article-type").ownText();
         blogInfo.setArticleType(articleType);
+
         // summary
-        String summary = element.selectFirst("p").ownText();
+        //String summary = element.selectFirst("p").ownText();
+        String summary = aElement.selectFirst("div.blog-list-content").ownText();
         blogInfo.setSummary(summary);
+
         // date
-        String date = element.selectFirst("div.info-box.d-flex.align-content-center > p > span").ownText();
+        String date = aElement.selectFirst("div.blog-list-footer > div.blog-list-footer-right").text();
         blogInfo.setDate(date);
+
         // readNum
-        Element readNumElement = element.selectFirst("div.info-box.d-flex.align-content-center > p > span.read-num");
+        Element readNumElement = aElement.selectFirst("div.blog-list-footer > div.blog-list-footer-left > div.view-num-box > span");
         if (readNumElement != null) {
             String readNum = readNumElement.ownText();
             blogInfo.setReadNum(Integer.valueOf(readNum));
         }
+
+
         // commentNum
-        Element commentNumElement = element.selectFirst("div.info-box.d-flex.align-content-center > p > span:nth-child(3)");
+        Element commentNumElement = aElement.selectFirst("div.blog-list-footer > div.blog-list-footer-left > div.comment-box > span");
         if (commentNumElement != null){
             String commentNum = commentNumElement.ownText();
             blogInfo.setCommentNum(Integer.valueOf(commentNum));
